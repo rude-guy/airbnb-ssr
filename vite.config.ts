92041f26b58f4c2718bffc20b59703e293f20bc5
rude-guy <2064825358.@qq.com>
2022-06-14 19:07:01 +0800

    over

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/release': {
-        target: 'http://localhost:9001',
+        target: 'http://49.235.70.227:9001',
         rewrite: path => path.replace(/^\/release/, '')
       }
     }
